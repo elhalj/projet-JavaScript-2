afc266e5a4df027f453fa4e8d4e8b8006f9e68a0
wilson ikeda <wilsonikedakoffi7@gmail.com>
2024-08-16 22:38:49 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * nstructions
+
+Fonctions de manipulation de chaînes :
+
+Inverser une chaîne : Écrivez une fonction qui inverse une chaîne donnée.
+Compter les caractères : créez une fonction qui compte le nombre de caractères d’une chaîne.
+Mettre les mots en majuscules : Implémentez une fonction qui met en majuscule la première lettre de chaque mot d’une phrase.
+Fonctions du tableau :
+
+Trouver le maximum et le minimum : Écrivez des fonctions pour trouver les valeurs maximum et minimum dans un tableau de nombres.
+Somme du tableau : créez une fonction qui calcule la somme de tous les éléments d’un tableau.
+Filter Array : implémente une fonction qui filtre les éléments d’un tableau en fonction d’une condition donnée.
+Fonctions mathématiques :
+
+Factorielle : Écrivez une fonction pour calculer la factorielle d’un nombre donné.
+Vérification des nombres premiers : Créez une fonction pour vérifier si un nombre est premier ou non.
+Séquence de Fibonacci : Implémentez une fonction pour générer la suite de Fibonacci jusqu’à un nombre donné de termes. (recherche sur le net )
+ */
